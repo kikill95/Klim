9ea169062119cf0a398a9acc5781d361affb3c51
kikill95 <kirill.gus@gmail.com>
2016-03-07 12:20:03 +0200

    Edit fonts; update resources; setting url for videos (adn whitelist plugin)

--- app/services/videos.ts
+++ app/services/videos.ts
@@ -6,35 +6,35 @@ export class VideoService {
         let videos = [
             {
                 title: 'Canned Lightning',
-                src: '../../../vids/Canned Lightning.mp4'
+                src: 'vids/canned_lightning.mp4'
             },
             {
                 title: 'Fireworks',
-                src: '../../../vids/Fireworks.mp4'
+                src: 'vids/fireworks.mp4'
             },
             {
                 title: 'Green Spark',
-                src: '../../../vids/Green Spark.mp4'
+                src: 'vids/green_spark.mp4'
             },
             {
                 title: 'Jellyfish',
-                src: '../../../vids/Jellyfish.mp4'
+                src: 'vids/jellyfish.mp4'
             },
             {
                 title: 'Cat',
-                src: '../../../vids/Cat.mp4'
+                src: 'vids/sat.mp4'
             },
             {
                 title: 'Minions - Banana Song',
-                src: '../../../vids/Minions - Banana Song.mp4'
+                src: 'vids/minions_banana_song.mp4'
             },
             {
                 title: 'Tron - Dance',
-                src: '../../../vids/Tron - Dance.mp4'
+                src: 'vids/tron_dance.mp4'
             },
             {
                 title: 'BB-8',
-                src: '../../../vids/BB-8.mp4'
+                src: 'vids/bb_8.mp4'
             }
         ];
         return videos;
